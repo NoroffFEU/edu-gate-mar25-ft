3d940edfcb48c8707aaa86c0713c57ff2cf5c34a
8headswillroll8 <lejohansen91@gmail.com>
2025-11-24 16:58:18 +0100

    Add html for landing page

--- pages/home.js
+++ pages/home.js
@@ -1,8 +1,12 @@
-export default function Home(){
-    return /*HTML*/`
+export default function Home() {
+  return /*HTML*/ `
     <div>
-    <h1>Testing home page</h1>
-    <p>Welcome to the home page!</p>
+    <h1>Edugate</h1>
+    <p>Welcome to Edugate, the platform that lets you access your exam results online.</p>
+   <img src="public/img/heroimage-edugate.png" alt="two-students-sitting-on-the-grass-with-laptops">
+   <button class="sign-up">Sign Up</button>
+   <button class="log-in">Log In</button>
     </div>  
+    
     `;
 }
