4e6d3d8c1b44b8a28a81202534c8838a266a8b00
8headswillroll8 <lejohansen91@gmail.com>
2025-11-25 19:58:45 +0100

    add media queries

--- pages/home.js
+++ pages/home.js
@@ -12,7 +12,7 @@ export default function Home() {
       </div>
 
       <div class="home__actions">
-        <a href="/signup" data-link class="btn btn--primary">Sign Up</a>
+        <a href="/signup" data-link class="btn">Sign Up</a>
         <a href="#" class="btn btn--secondary">Log In</a>
       </div>
     </section>  
