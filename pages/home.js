e32cbebc903299b1bf4e0f1b61912ec5e2518071
8headswillroll8 <lejohansen91@gmail.com>
2025-11-25 18:04:52 +0100

    rebuild layout

--- pages/home.js
+++ pages/home.js
@@ -1,7 +1,22 @@
 export default function Home() {
   return /*HTML*/ `
-    <section class="hero-container">
-    
+    <section class="home__container">
+
+      <figure class="home__media">
+      <img src="/public/img/heroimage-edugate.png" alt="two-students-sitting-on-the-grass-with-laptops">
+      </figure>
+
+      <div class="home__content">
+      <h1>Edugate</h1>
+      <p>Welcome to Edugate, the platform that lets you access your exam results online.</p>
+      </div>
+
+      <div class="home__btn">
+      <a href="/signup" data-link>Sign Up</a>
+      <a href="#">Log In</a>
+      </div>
+
+      .home-
     </section>  
 
     `;
