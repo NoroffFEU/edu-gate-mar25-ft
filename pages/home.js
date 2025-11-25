ecd2edb8fa487eddab35dc3440bdc98b29fe572c
8headswillroll8 <lejohansen91@gmail.com>
2025-11-25 13:46:35 +0100

    Reset layout due to incorrect breakpoints

--- pages/home.js
+++ pages/home.js
@@ -1,21 +1,8 @@
 export default function Home() {
   return /*HTML*/ `
-    <div class="hero-container">
-
-      <div class="hero-img-wrapper">
-        <img src="public/img/heroimage-edugate.png" alt="two-students-sitting-on-the-grass-with-laptops" class="hero-img">
-      </div>
-
-      <div class="hero-content-wrapper">
-        <h1>Edugate</h1>
-        <p>Welcome to Edugate, the platform that lets you access your exam results online.</p>
-      </div>
-      
-      <div class="hero-btn-wrapper">
-        <button class="btn">Sign Up</button>
-        <button class="btn btn--secondary">Log In</button>
-      </div>
-    </div>  
+    <section class="hero-container">
+    
+    </section>  
 
     `;
 }
