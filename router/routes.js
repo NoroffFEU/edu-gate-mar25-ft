fd2f9ad0fff3d2d3c07a8138e8af583829a2bf52
Nicklasoeen <nicklas-oeen@hotmail.com>
2025-11-26 12:38:28 +0100

    added routing

--- router/routes.js
+++ router/routes.js
@@ -2,11 +2,13 @@
 import Home from '../pages/home.js'
 import About from '../pages/about.js'
 import Login from '../pages/login.js'
+import StudentDashboard from '../pages/student-dashboard.js'
 
 const routes = [
     { path: "/", view: Home },
     { path: "/about", view: About },
     { path: "/login", view: Login },
+    { path: "/student-dashboard", view: StudentDashboard },
 ];
 
 export default routes;
