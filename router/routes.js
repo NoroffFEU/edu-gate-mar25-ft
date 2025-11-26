7455f75abfc4ebe1f4bcdcca359b956e659f8f0d
Nicklasoeen <nicklas-oeen@hotmail.com>
2025-11-26 12:38:28 +0100

    added routing

--- router/routes.js
+++ router/routes.js
@@ -2,22 +2,24 @@
 
 import Home from "../pages/home.js";
 import About from "../pages/about.js";
-import Login from "../pages/login.js";
+import Login, { initLoginForm } from "../pages/login.js";
 import StudentResults from "../pages/student-results.js";
 import AdminDashboard from "../pages/admin-dashboard.js";
 import Signup, { initSignupForm } from "../pages/signup.js";
 import TeacherDashboard from "../pages/teacher-dashboard.js";
 import Registration from "../pages/registration.js";
+import StudentDashboard from '../pages/student-dashboard.js'
 
 const routes = [
  { path: "/", view: Home },
  { path: "/about", view: About },
- { path: "/login", view: Login },
+ { path: "/login", view: Login, afterRender: initLoginForm },
  { path: "/student-results", view: StudentResults },
  { path: "/admin-dashboard", view: AdminDashboard },
  { path: "/signup", view: Signup, afterRender: initSignupForm },
  { path: "/teacher-dashboard", view: TeacherDashboard },
  { path: "/registration", view: Registration },
+ { path: "/student-dashboard", view: StudentDashboard },
 ];
 
 export default routes;
