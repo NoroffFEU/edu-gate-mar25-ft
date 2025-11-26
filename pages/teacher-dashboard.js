766178b41fde83e3513bfc0e14d1d177b54389df
tominehaug <tomine_haug@live.no>
2025-11-26 20:35:27 +0100

    updates

--- pages/teacher-dashboard.js
+++ pages/teacher-dashboard.js
@@ -10,22 +10,36 @@ export default function teacherDashboard(){
             <li><a href=""><img src="../public/icons/logout.png" alt="logout icon"></a></li>
         </ul>
     </nav>
-    <h1>Dashboard</h1>
-        <img src="../public/img/Student initials.png" alt="student initials icon">
-        <p>Joe Bloggs</p>
-        <p>Teacher at Edugate school</p>
+    <div main-content>
+    <div class="user-info">
+        <h1>Dashboard</h1>
+            <img src="../public/img/Student initials.png" alt="student initials icon">
+            <h2>Joe Bloggs</h2>
+            <p>Teacher at Edugate school</p>
+    </div>
         <div class="options">
-            <img src="../public/icons/top-students.png" alt="top students icon">
-            <p>Top students</p>
-            <img src="../public/icons/add-results.png" alt="add results icon">
-            <p>Add results</p>
-            <img src="../public/icons/results.png" alt="results icon">
-            <p>View results</p>
-            <img src="../public/icons/profile-icon.png" alt="profile icon">
-            <p>See profile</p>
-            <img src="../public/icons/logout.png" alt="log out icon">
-            <p>Log out</p>
+            <div class="option">
+                <img src="../public/icons/top-students.png" alt="top students icon">
+                <h3>Top students</h3>
+            </div>
+            <div class="option">
+                <img src="../public/icons/add-results.png" alt="add results icon">
+                <h3>Add results</h3>
+            </div>
+            <div class="option">
+                <img src="../public/icons/results.png" alt="results icon">
+                <h3>View results</h3>
+            </div>
+            <div class="option">
+                <img src="../public/icons/user-circle.png" alt="profile icon">
+                <h3>See profile</h3>
+            </div>
+            <div class="option">
+                <img src="../public/icons/logout.png" alt="log out icon">
+                <h3>Log out</h3>
+            </div>
         </div>
+    </div>
     </div>`
 }
 
