22c1e2e138b0af4c6e633cc393d5f1991a2d62e6
DYNNIwav <paalomei@gmail.com>
2025-11-27 22:52:59 +0100

    Add student results page and routing
    Introduces a new student results page with table, search, and pagination UI. Adds corresponding CSS for styling and updates the router to include the new route. Also links the new CSS file in index.html.

--- router/routes.js
+++ router/routes.js
@@ -2,11 +2,13 @@
 import Home from '../pages/home.js'
 import About from '../pages/about.js'
 import Login from '../pages/login.js'
+import StudentResults from '../pages/student-results.js'
 
 const routes = [
     { path: "/", view: Home },
     { path: "/about", view: About },
     { path: "/login", view: Login },
+    { path: "/student-results", view: StudentResults },
 ];
 
 export default routes;
