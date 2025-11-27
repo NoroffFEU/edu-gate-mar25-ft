cad9970edd4a24f6947d92730178e3e3ddaec771
ND-Nitro <steffenheltne@hotmail.com>
2025-11-27 09:36:12 +0100

    made the htmml and css for admin mobile and desktop

---
+++ pages/admin-dashboard.js
@@ -0,0 +1,38 @@
+export default function AdminDashboard() {
+  return /*HTML*/ `
+<main class="dashboard">
+
+  <section class="admin-info">
+  <h1>Dashboard</h1>
+    <div class="avatar">JB</div>
+    <h2 class="admin-name">John Brown</h2>
+    <p class="admin-role">Admin at Edugate School</p>
+  </section>
+
+  <nav class="dashboard-nav"> 
+
+    <a href="/users" class="nav-item Manage Users">
+    <img src="./public/icons/user-circle.png" alt="manage users icon">
+    <p>Manage Users</p> </a>
+
+    <a href="/students/create" class="nav-item add-student">
+    <img src="./public/icons/add-student.png" alt="add student icon">
+    <p>Add Student</p> </a>
+
+    <a href="/teachers/create" class="nav-item add-teacher">
+    <img src="./public/icons/add-teacher.png" alt="add teacher icon">
+    <p>Add Teacher</p> </a>
+
+    <a href="/schools/create" class="nav-item add-school">
+    <img src="./public/icons/add-school.png" alt="add school icon">
+    <p>Add School</p> </a>
+
+    <a href="/logout" class="nav-item logout">
+    <img src="./public/icons/logout.png" alt="logout icon">
+    <p>Logout</p> </a>
+
+  </nav>
+
+  </main>
+  `;
+}
--- router/routes.js
+++ router/routes.js
@@ -1,12 +1,14 @@
 // views
-import Home from '../pages/home.js'
-import About from '../pages/about.js'
-import Login from '../pages/login.js'
+import Home from "../pages/home.js";
+import About from "../pages/about.js";
+import Login from "../pages/login.js";
+import AdminDashboard from "../pages/admin-dashboard.js";
 
 const routes = [
-    { path: "/", view: Home },
-    { path: "/about", view: About },
-    { path: "/login", view: Login },
+  { path: "/", view: Home },
+  { path: "/about", view: About },
+  { path: "/login", view: Login },
+  { path: "/admin-dashboard", view: AdminDashboard },
 ];
 
 export default routes;
