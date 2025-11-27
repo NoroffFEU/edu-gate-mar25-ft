e3c456460bb39d3970e0997a37a11ad135fa6d94
tominehaug <tomine_haug@live.no>
2025-11-27 13:31:55 +0100

    added route

--- router/routes.js
+++ router/routes.js
@@ -2,11 +2,13 @@
 import Home from '../pages/home.js'
 import About from '../pages/about.js'
 import Login from '../pages/login.js'
+import TeacherDashboard from '../pages/teacher-dashboard.js';
 
 const routes = [
     { path: "/", view: Home },
     { path: "/about", view: About },
     { path: "/login", view: Login },
+    { path: "/teacher-dashboard", view: TeacherDashboard }
 ];
 
 export default routes;
