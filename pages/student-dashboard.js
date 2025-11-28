a548ba3c237ce452275d65e52cb32476d3aa09d7
Nicklasoeen <nicklas-oeen@hotmail.com>
2025-11-28 10:19:11 +0100

    Changed p tag to h2

--- pages/student-dashboard.js
+++ pages/student-dashboard.js
@@ -9,21 +9,21 @@ export default function StudentDashboard() {
                 <div class="avatar-circle">
                     <img src="public/icons/profile-icon.png" alt="User Icon" class="avatar-icon"/>
                 </div>
-                <p class="avatar-name">Joe Bloggs</p>
+                <h2 class="avatar-name">Joe Bloggs</h2>
                 <p class="avatar-school">Student at Edugate school</p>
             </div>
           </div>
           <div class="dashboard-options">
-          <div class ="profile-container">
+          <div class ="logo-container">
             <img src="public/icons/user-circle.png" alt="Profile Icon" class="profile-icon"/>
             <a href="/#" class="student-profile">See profile</a>
           </div>
-        <div class ="result-container">
+        <div class ="logo-container">
         <img src="public/icons/results.png" alt="Profile Icon" class="profile-icon"/>
         <a href="/#" class="student-results">See results</a>
         </div>
 
-        <div class ="logout-container">
+        <div class ="logo-container">
             <img src="public/icons/logout.png" alt="Logout Icon" class="profile-icon"/>
             <a href="/#" class="student-logout">Logout</a></div>
          </div>
