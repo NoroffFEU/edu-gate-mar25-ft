d489ed5eeee803aebff8d6a85b626691ce788d88
anthonyapitchou <anthoapicella@hotmail.fr>
2025-12-01 17:30:32 +0100

    fix side bar

--- app.js
+++ app.js
@@ -2,13 +2,17 @@
 import { initRouter } from "./router/router.js";
 import Header from "./components/header.js";
 import Footer from "./components/footer.js";
-import SidebarAdmin from "./components/sidebar-admin.js";
 import SidebarTeaching from "./components/sidebar-teaching.js";
+import SidebarAdmin from "./components/sidebar-admin.js";   
+
 
 
 
 document.getElementById("header").innerHTML = Header();
 document.getElementById("footer").innerHTML = Footer();
-document.getElementById("sidebar").innerHTML = SidebarTeaching();
+document.getElementById("sidebarTeacher").innerHTML = SidebarTeaching();
+document.getElementById("sidebarAdmin").innerHTML = SidebarAdmin(); 
 
 initRouter();
+
+
