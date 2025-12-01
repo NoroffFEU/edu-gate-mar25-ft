99d84200db5f6334bd537f12707ab21c0f1469b0
8headswillroll8 <lejohansen91@gmail.com>
2025-12-01 17:35:40 +0100

    add aria-labels and impove accessibility

--- pages/home.js
+++ pages/home.js
@@ -18,13 +18,13 @@ export default function Home() {
     </section>  
   </div>
 
-  <!-- Temporary alert test setup in home.js. Final placement goes to login.js and signup.js after merge -->
-  <div class="alert">
+  <!-- temporary alert component used for testing in home.js only. After merge, move the final implementation to login.js and signup.js -->
+  <div class="alert alert--success" role="alert" aria-live="assertive">
     <div class="alert-text">
-      <h2 class="alert-title">Failed Sign Up!</h2>
-      <p class="alert-message">Wrong password or email address!</p>
+      <h2>Success!!</h2>
+      <p>Please check your email for instructions on how to verify your account.</p>
     </div>
-    <button class="alert-close"><img src="/public/icons/x-red.png" alt="x symbol"></button>
+    <button class="alert-close" type="button" aria-label="Close alert"><img src="/public/icons/green-x.png" alt=""></button> <!-- change the icon depending on the alert -->
   </div>
 
     `;
