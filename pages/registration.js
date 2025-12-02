3261cf6a9980ade0f4b789e207df58c44cfdd2a4
Anna Vestman <Anna.v.westman@gmail.com>
2025-12-02 10:29:41 +0100

    Created new pages, js, css, routes

---
+++ pages/registration.js
@@ -0,0 +1,48 @@
+export default function Registration() {
+ return /*HTML*/ `
+<section class="registrationPage">
+      <div class="registrationCard">
+      
+        <h1 class="page-title">
+            <span class="mobile">Registration</span>
+            <span class="desktop">Register</span>
+        </h1>
+
+        <form class="registrationForm">
+          <div class="form-group">
+            <label for="name" class="form-group-label">School</label>
+            <input id="schoolInput" type="text"
+            placeholder="Edugate" required />
+          </div>
+
+          <div class="form-group">
+            <label for="Address"class="form-group-label">Address line 1</label>
+            <input id="addressInput1" type="text"
+            placeholder="Address" required />
+          </div>
+
+           <div class="form-group">
+            <label for="Address"class="form-group-label">Address line 2</label>
+            <input id="addressInput2" type="text"
+            placeholder="Address" required />
+          </div>
+
+        <div class="city-group">
+          <div class="form-group">
+            <label for="City"class="form-group-label">City</label>
+            <input id="cityInput" type="text"
+            placeholder="Oslo" required />
+          </div>
+
+          <div class="form-group">
+            <label for="postalCode"class="form-group-label">Postal Code</label>
+            <input id="postalCodeInput" type="number" 
+            placeholder="0000" required />
+          </div>
+          </div>
+
+          <button type="submit" class="primary-btn regBtn">Submit</button>
+        </form>
+      </div>
+  `;
+}
--- router/routes.js
+++ router/routes.js
@@ -3,12 +3,14 @@ import Home from "../pages/home.js";
 import About from "../pages/about.js";
 import Login from "../pages/login.js";
 import AdminDashboard from "../pages/admin-dashboard.js";
+import Registration from "../pages/registration.js";
 
 const routes = [
-  { path: "/", view: Home },
-  { path: "/about", view: About },
-  { path: "/login", view: Login },
-  { path: "/admin-dashboard", view: AdminDashboard },
+ { path: "/", view: Home },
+ { path: "/about", view: About },
+ { path: "/login", view: Login },
+ { path: "/admin-dashboard", view: AdminDashboard },
+ { path: "/registration", view: Registration },
 ];
 
 export default routes;
