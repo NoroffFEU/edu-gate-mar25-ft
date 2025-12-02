c424258fb823bab1d3f9ac462fc2fc34619ef826
8headswillroll8 <lejohansen91@gmail.com>
2025-12-02 18:27:24 +0100

    final push to git

--- components/alert.js
+++ components/alert.js
@@ -1,12 +1,12 @@
 export default function Alert(type, title, message) {
   return /* HTML */ `
-    <div class="alert alert--failed" role="alert" aria-live="assertive">
+    <div class="alert alert--${type}" role="alert" aria-live="assertive">
       <div class="alert-text">
-        <h2>Failed Sign Up!</h2>
-        <p>Wrong password or email address!</p>
+        <h2>${title}</h2>
+        <p>${message}</p>
       </div>
       <button class="alert-close" type="button" aria-label="Close alert">
-        <img src="/public/icons/x-red.png" alt="" />
+        <img src="/public/icons/x-${type}.png" alt="" />
       </button>
     </div>
   `;
