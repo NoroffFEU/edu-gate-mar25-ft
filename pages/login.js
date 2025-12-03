87462b4d17bdfc4553a16d8b8a0326930389ff8b
moaisason <m.isaksson@hotmail.com>
2025-12-03 11:44:01 +0100

    Login page for desktop and mobile #11

--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,25 @@
 export default function Login(){
+    
     return /*HTML*/`
-    <div>
-    <h1>Login Page</h1>
-    <p>Please enter your credentials to log in.</p>
-    <a href="/" data-link>Home</a>
+    
+    <div class="login-page">
+       <div class="login-form">
+            <h1>Log in</h1>
+            <div class="login-input">
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email" placeholder="example@mail.com">
+            </div>
+            <div class="login-input">
+                <label for="password">Password</label>
+                <input type="text" name="password" id="password" placeholder="Enter password">
+            </div>
+            <p>Forgot your password? Click 
+                <a href="#">here</a>
+            </p>
+            <a href="#">
+                <button class="btn">Submit</button>
+            </a>
+        </div>
     </div>  
     `;
 }
