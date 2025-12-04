f5e3181c353443655f3381bfb0bfb6e2fa787209
Elljens <ellen.jensen81@gmail.com>
2025-12-04 18:47:37 +0100

    Added four different sidepanels for hamburger menus to components

--- components/header.js
+++ components/header.js
@@ -1,7 +1,6 @@
 export default function Header() {
   return /*HTML*/ `
-    <p>Header</p>
-    <a href="/" data-link>Home</a> |
-    <a href="/login" data-link>Login</a> here
+    <img src="./public/icons/hamburger-menu.png" class="hamburgerMenu">
+
   `;
 }
