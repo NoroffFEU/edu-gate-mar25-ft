98049346565b23ccfba8060afaafcb62f9a21638
moaisason <m.isaksson@hotmail.com>
2025-12-04 18:35:12 +0100

    Class-names changed

--- pages/login.js
+++ pages/login.js
@@ -4,19 +4,19 @@ export default function Login(){
     
     <div class="login-page">
        <div class="login-form">
-            <h1>Log in</h1>
-            <div class="login-input">
-                <label for="email">Email</label>
-                <input type="email" name="email" id="email" placeholder="example@mail.com">
+            <h1 class="login-h1" >Log in</h1>
+            <div class="login-input-form">
+                <label class="login-label" for="email">Email</label>
+                <input class="login-input" type="email" name="email" id="email" placeholder="example@mail.com">
             </div>
-            <div class="login-input">
-                <label for="password">Password</label>
-                <input type="text" name="password" id="password" placeholder="Enter password">
+            <div class="login-input-form">
+                <label class="login-label" for="password">Password</label>
+                <input class="login-input" type="text" name="password" id="password" placeholder="Enter password">
             </div>
-            <p>Forgot your password? Click 
-                <a href="#">here</a>
+            <p class="login-p">Forgot your password? Click 
+                <a class="login-a" href="#">here</a>
             </p>
-            <a href="#">
+            <a class="login-a" href="#">
                 <button class="btn">Submit</button>
             </a>
         </div>
