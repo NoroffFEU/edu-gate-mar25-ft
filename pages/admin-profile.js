ea5aafdeb5e0b3e5a046f01f980000e1fa0835cf
moaisason <m.isaksson@hotmail.com>
2025-12-04 17:24:35 +0100

    Solved conflicts

---
+++ pages/admin-profile.js
@@ -0,0 +1,16 @@
+export default function AdminProfile() {
+  return /*HTML*/ `
+    <div>
+    Dashboard
+    img
+    Profile
+        <div>
+    <h1>Profile</h1>
+    <img src= "../public/icons/profile-icon.png" />
+    <p></p>
+    <a href="/" data-link>Home</a>
+    <button>Edit Profile</button>
+    </div>
+    </div>
+    `;
+}
