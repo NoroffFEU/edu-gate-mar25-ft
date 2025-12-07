9bdba294c90549f5fdae870e6eccf29921df6ec1
tominehaug <tomine_haug@live.no>
2025-12-07 11:31:52 +0100

    formatting

--- components/sidebar-admin.js
+++ components/sidebar-admin.js
@@ -21,35 +21,36 @@ export default function SidebarAdmin() {
   <button type="button" id="sidebar-button">
     <img src="./public/icons/green-double-arrows-right.png" alt="sidebar toggle button">
   </button>
-<div id="sidebar">
-  <div>
-     <a href="#home" class="logo" title="Home">
-      <img class="icon" src="./public/icons/dashboard.png" alt="home-logo">
-      <span class="label">Home</span>
-    </a>
-  </div>
-  <div>
-    <a href="#result-management" class="logo" title="Results management">
-      <img class="icon" src="./public/icons/add-results.png" alt="management-logo">
-      <span class="label">Results management</span>
-    </a>
-  <div>
-    <a href="#user-management" class="logo" title="User management">
-      <img class="icon" src="./public/icons/user-management.png" alt="user-management-logo">
-      <span class="label">Users</span>
-    </a>
-  </div>
-  <div>
-    <a href="#top-students" class="logo" title="school management">
-      <img class="icon" src="./public/icons/school-management.png" alt="school management-logo">
-      <span class="label">School management</span>
-    </a>
-  </div>
-  <div>
-    <a href="#logout" class="logo" title="Logout">
-      <img class="icon" src="./public/icons/logout.png" alt="logout-logo">
-      <span class="label">Logout</span>
-    </a>
+  <div id="sidebar">
+    <div>
+      <a href="#home" class="logo" title="Home">
+        <img class="icon" src="./public/icons/dashboard.png" alt="home-logo">
+        <span class="label">Home</span>
+      </a>
+    </div>
+    <div>
+      <a href="#result-management" class="logo" title="Results management">
+        <img class="icon" src="./public/icons/add-results.png" alt="management-logo">
+        <span class="label">Results management</span>
+      </a>
+    <div>
+      <a href="#user-management" class="logo" title="User management">
+        <img class="icon" src="./public/icons/user-management.png" alt="user-management-logo">
+        <span class="label">Users</span>
+      </a>
+    </div>
+    <div>
+      <a href="#top-students" class="logo" title="school management">
+        <img class="icon" src="./public/icons/school-management.png" alt="school management-logo">
+        <span class="label">School management</span>
+      </a>
+    </div>
+    <div>
+      <a href="#logout" class="logo" title="Logout">
+        <img class="icon" src="./public/icons/logout.png" alt="logout-logo">
+        <span class="label">Logout</span>
+      </a>
+    </div>
   </div>
 </div>
   `;
