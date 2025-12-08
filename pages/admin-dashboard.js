44828927dbaac53f9e8f43aa445f2a819a3d0e25
tominehaug <tomine_haug@live.no>
2025-12-08 13:41:50 +0100

    updated containers for styling

--- pages/admin-dashboard.js
+++ pages/admin-dashboard.js
@@ -2,8 +2,9 @@ import SidebarAdmin from "../components/sidebar-admin.js";
 
 export default function AdminDashboard() {
   return /*HTML*/ `
-<main class="dashboard">
+<div>
 <div id="sidebar-container">${SidebarAdmin()}</div>
+<div class="dashboard">
   <section class="admin-info">
   <h1>Dashboard</h1>
   
@@ -40,7 +41,7 @@ export default function AdminDashboard() {
     <p>Logout</p> </a>
 
   </nav>
-
-  </main>
+  </div>
+  </div>
   `;
 }
