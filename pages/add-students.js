e3d236db120b00951f316cf59426b20e7eba70b0
tominehaug <tomine_haug@live.no>
2025-12-10 12:11:22 +0100

    write and export html

---
+++ pages/add-students.js
@@ -0,0 +1,18 @@
+export default function AddStudent() {
+  return /*HTML*/ `
+    <div>
+        <div class="breadcrumbs">
+            <p>Dashboard</p>
+            <p>></p>
+            <p>Add students</p>
+        </div>
+        <div class="main-content">
+        <h1>Add student</h1>
+        <p>To add students, upload an Excel spreadsheet with this format:</p>
+        <div id="excel-sheet"></div>
+        <button type="button" class="primary-btn">Upload +</button>
+        <button type="button" class="secondary-btn">Back to dashboard</button>
+        </div>
+    </div>
+    `;
+}
