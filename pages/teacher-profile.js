fbabee9b620c6ef28aaaa1202514b865869d5d5b
8headswillroll8 <lejohansen91@gmail.com>
2025-12-10 22:05:25 +0100

    add html structure

--- pages/teacher-profile.js
+++ pages/teacher-profile.js
@@ -1,6 +1,43 @@
 export default function TeacherProfilePage() {
   return /*HTML*/ `
-  <section>
+  <section class="profile-card">
+    <div class="profile-card__wrapper">
+      <h2>Profile</h2>
+
+      <div class="profile-avatar">JB</div>
+
+      <div class="profile-field">
+        <span class="profile-label">Name:</span>
+        <span class="profile-value">${}</span>
+      </div>
+
+      <div class="profile-field">
+        <span class="profile-label">Email:</span>
+        <span class="profile-value">${}</span>
+      </div>
+
+      <div class="profile-field">
+        <span class="profile-label">Date of birth:</span>
+        <span class="profile-value">${}</span>
+      </div>
+
+      <div class="profile-field">
+        <span class="profile-label">ID:</span>
+        <span class="profile-value">${}</span>
+      </div>
+
+      <div class="profile-field">
+        <span class="profile-label">Position:</span>
+        <span class="profile-value">${}</span>
+      </div>
+
+      <div class="profile-field">
+        <span class="profile-label">School:</span>
+        <span class="profile-value">${}</span>
+      </div>
+
+      <button class="secondary-btn">Edit Profile</button>
+    </div>
   </section>
    `;
 }
