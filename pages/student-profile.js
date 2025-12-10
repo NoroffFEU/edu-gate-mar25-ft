d3a5244c12a7a0c83866cb3dbb1feafdfabe38b4
naima <naima.abdullahi@hotmail.com>
2025-12-10 18:36:20 +0100

    corrected html tags

--- pages/student-profile.js
+++ pages/student-profile.js
@@ -1,4 +1,4 @@
-export default function StudentProfile() {
+export default function AdminDashboard() {
   return /*HTML*/ `
     <div class = "profile-page">
         <nav class = "profile-breadcrumb">
@@ -13,33 +13,33 @@ export default function StudentProfile() {
 
         <div class = "profile-details"> 
             <div class = "profile-row">
-                <span class = "profile-label">Name: </span>
-                <span class = "profile-value">Joe Bloggs </span>
+                <p class = "profile-label">Name: </p>
+                <p class = "profile-value">Joe Bloggs </p>
             </div> 
 
             <div class = "profile-row">
-                <span class = "profile-label">Email: </span>
-                <span class = "profile-value"> joeblog2024@edugate.no </span>
+                <p class = "profile-label">Email: </p>
+                <p class = "profile-value"> joeblog2024@edugate.no </p>
             </div>
 
             <div class = "profile-row">
-                <span class = "profile-label">DOB: </span>
-                <span class =  "profile-value">01/01/2002</span>
+                <p class = "profile-label">DOB: </p>
+                <p class =  "profile-value">01/01/2002 </p>
             </div>
 
             <div class = "profile-row">
-                <span class = "profile-label">ID: </span>
-                <span class = "profile-value">1921840</span>
+                <p class = "profile-label">ID: </p>
+                <p class = "profile-value">1921840 </p>
             </div>
 
             <div class = "profile-row">
-                <span class = "profile-label">Grad Year: </span>
-                <span class = "profile-value">2020</span>
+                <p class = "profile-label">Grad Year: </p>
+                <p class = "profile-value">2020 </p>
             </div>
 
             <div class = "profile-row">
-                <span class = "profile-label">School: </span>
-                <span class = "profile-value">The Academy</span>
+                <p class = "profile-label">School: </p>
+                <p class = "profile-value">The Academy </p>
             </div>
 
         </div>
