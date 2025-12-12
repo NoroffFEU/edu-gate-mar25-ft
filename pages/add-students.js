3f3b43b6b122b1db19988a2c14b3abb99562ef1f
tominehaug <tomine_haug@live.no>
2025-12-12 13:22:37 +0100

    changed path in export

--- pages/add-students.js
+++ pages/add-students.js
@@ -1,5 +1,5 @@
 import SidebarTeacher from "../components/sidebar-teaching.js";
-import * as XLSX from "xlsx";
+import * as XLSX from "../xlsx/xlsx.js";
 
 export default function AddStudent() {
   setTimeout(() => {
