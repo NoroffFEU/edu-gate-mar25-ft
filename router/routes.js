eee6315d29bc74aaec22271301544d7cd3be9657
ND-Nitro <steffenheltne@hotmail.com>
2025-12-15 00:07:13 +0100

    added routing

--- router/routes.js
+++ router/routes.js
@@ -1,12 +1,14 @@
 // views
-import Home from '../pages/home.js'
-import About from '../pages/about.js'
-import Login from '../pages/login.js'
+import Home from "../pages/home.js";
+import About from "../pages/about.js";
+import Login from "../pages/login.js";
+import TopPerformingStudents from "../pages/topperformingstudents.js";
 
 const routes = [
-    { path: "/", view: Home },
-    { path: "/about", view: About },
-    { path: "/login", view: Login },
+  { path: "/", view: Home },
+  { path: "/about", view: About },
+  { path: "/login", view: Login },
+  { path: "/topperformingstudents", view: TopPerformingStudents },
 ];
 
 export default routes;
