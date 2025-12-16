c8ac703da6f2b25d7ad7b95ec3f837dce675f64f
tominehaug <tomine_haug@live.no>
2025-12-16 15:42:16 +0100

    import parser

--- pages/add-students.js
+++ pages/add-students.js
@@ -1,80 +1,52 @@
 import SidebarTeacher from "../components/sidebar-teaching.js";
+import parser from "@jspreadsheet/parser";
 
 export default function AddStudent() {
-  const sheetStyles = document.createElement("link");
-  sheetStyles.href =
-    "https://cdn.jsdelivr.net/npm/jspreadsheet-ce@4.15.0/dist/jspreadsheet.css";
-  sheetStyles.rel = "stylesheet";
-
-  const xlsxScript = document.createElement("script");
-  xlsxScript.src =
-    "https://cdn.jsdelivr.net/npm/jspreadsheet-ce@4.15.0/dist/jspreadsheet.js";
-
-  const parseXlsx = document.createElement("script");
-  parseXlsx.src = "https://cdn.jsdelivr.net/npm/xlsx/dist/xlsx.full.min.js";
-
-  document.head.appendChild(sheetStyles);
-  document.head.appendChild(xlsxScript);
-  document.head.appendChild(parseXlsx);
-
   setTimeout(() => {
-    // display spreadsheet with example input when site is loaded
-    const spreadsheet = document.getElementById("spreadsheet");
-    const exampleData = [
-      [
-        "First Name",
-        "Second Name",
-        "Year of Graduation",
-        "School Student ID Number",
-        "Date of Birth",
-      ],
-    ];
+    const spreadsheetInfo = document.getElementById("spreadsheet-info");
+    const uploadBtn = document.getElementById("uploadBtn");
+    let parsedData = null;
 
-    console.log(xlsx);
+    // parse excel-file
 
-    let ws = xlsx.utils.aoa_to_sheet(exampleData);
-    const html = xlsx.utils.sheet_to_html(ws);
 
-    spreadsheet.innerHTML = html;
 
-    // upload and validate xlsx-file
-    // switch out displayed spreadsheet to the one uploaded by user
+    document.getElementById("excelFile")
+      .addEventListener("change", async (event) => {
+        const fileInput = event.target.files[0];
+        if(!fileInput) return;
+
+        try{
+
+        }
+      });
+
+    // validation logic
+
+    // send input to server
     // display error messages
-    // interact with the spreadsheet to fix errors
-    // click upload button to update after error handling
-    // validate again and send input to server
-
-    // spreadsheet,
-    //   {
-    //     data: exampleData,
-    //     columns: [
-    //       { type: "text", title: "first_name" },
-    //       { type: "text", title: "surname" },
-    //       { type: "number", title: "grad_year" },
-    //       { type: "number", title: "id" },
-    //       { type: "date", title: "dob" },
-    //     ],
-    //   };
-  }, 0);
+    // display success message
+  });
 
   return /*HTML*/ `
     <div>
     <div id="sidebar-container">${SidebarTeacher()}</div>
-        <div class="breadcrumbs">
-            <p>Dashboard</p>
-            <p>></p>
-            <p>Add students</p>
-        </div>
-        <div class="main-content">
+      <div class="breadcrumbs">
+        <p>Dashboard</p>
+        <p>></p>
+        <p>Add students</p>
+      </div>
+      <div class="main-content">
         <h1>Add student</h1>
         <p>To add students, upload an Excel spreadsheet with this format:</p>
-        <div>
-            <input type="file" id="uploadExcel" accept=".xlsx, .xls" />
-            <div id="spreadsheet"></div>
-            <button type="button" id="uploadBtn" class="primary-btn">Upload +</button>
+        <div id="spreadsheet-info">
+          <img src="../public/img/excel_table_desktop.png" class="example-table-desktop">
+          <img src="../public/img/excel_table_mobile.png" class="example-table-mobile"> 
         </div>
+        <input type="file" id="excelFile" accept=".xlsx, .xls" />
+        <button type="button" id="uploadBtn" class="primary-btn">Upload +</button>
         <button type="button" class="secondary-btn">Back to dashboard</button>
-        </div>
+      </div>
     </div>
     `;
 }
