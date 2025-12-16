621bc73208bf219807363d5a6e16bde24422a8fb
naima <naima.abdullahi@hotmail.com>
2025-12-16 15:00:31 +0100

    - Deleted the literal word «here» from header.js - Changed max-width on .profile-page from 361px to 600px - Did changes on .profile__row,  .profile-label and .profile-value so long values ex. (Email) stay on columns

--- components/header.js
+++ components/header.js
@@ -2,7 +2,7 @@ export default function Header() {
   return /*HTML*/ `
     <p>Header</p>
     <a href="/" data-link>Home</a> |
-    <a href="/login" data-link>Login</a> here
+    <a href="/login" data-link>Login</a> 
     <a href="/student-profile" data-link>Student profile</a>
   `;
 }
