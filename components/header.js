ee6cf634302e92cf2bd71cb725d2d6292913e3bc
naima <naima.abdullahi@hotmail.com>
2025-12-17 10:40:30 +0100

    removed  the literal word "header" from header.js

--- components/header.js
+++ components/header.js
@@ -50,7 +50,6 @@ export default function Header() {
 
   });
   return /*HTML*/ `
-    <p>Header</p>
     <a href="/" data-link>Home</a> |
     <a href="/login" data-link>Login</a> 
     <a href="/student-profile" data-link>Student profile</a>
