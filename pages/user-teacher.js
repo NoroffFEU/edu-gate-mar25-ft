a14d5d0fda54b3d996346718d8584ddc7dcd8343
naima <naima.abdullahi@hotmail.com>
2025-12-18 17:57:30 +0100

    created the first part of html

--- pages/user-teacher.js
+++ pages/user-teacher.js
@@ -6,9 +6,46 @@ export default function UserTeacher(){
         <span>Select user</span>
         <span>&gt;</span>
         <span class = "profile-breadcrumb-active"> Manager user </span>
-        <p>Edit the user info below:</p>
     </nav> 
 
+    <nav>
+        <h1> Manage User </h1>
+        <p>Edit the user info below:</p>
+    </nav>
+
+   <section class = "profile__card">
+
+    <form>
+        <div>
+            <label for="id">ID:</label>
+            <input id= "id" name= "id" type="text" value="1921840" />
+        </div>
+        <div>
+            <label for="firstName">First name:</label>
+            <input id= "firstName" name= "firstName" type="text" value="Joe"  />
+        </div>
+        <div>
+            <label for="lastName">Last name:</label>
+            <input id= "lastName" name= "lastName" type="text" value="Bloggs"  />
+        </div>
+        <div>
+            <label for="position">Position:</label>
+            <input id= "position" name= "position" type="text" value="Teacher" />
+        </div>
+        <div>
+            <label for="dob">DOB:</label>
+            <input id= "dob" name= "dob" type="text" value="19/11/1988" />
+        </div>
+        <div>
+            <label for="email">Email:</label>
+            <input id= "email" name= "email" type="text" value="joeblog2020@edugate.no" />
+        </div>
+        <div>
+            <label for="school">School:</label>
+            <input id= "school" name= "school" type="text" value="The Academy" />
+        </div>
+    </form>
+   </section>
 `;
 
 }
