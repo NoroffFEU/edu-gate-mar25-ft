891f2a550c4bf678fd3a4574cfe700dbb4a6b4ad
naima <naima.abdullahi@hotmail.com>
2025-12-18 17:09:23 +0100

    added the html for the breadcrumb

--- pages/user-teacher.js
+++ pages/user-teacher.js
@@ -1,6 +1,13 @@
 export default function UserTeacher(){
     return /*HTML*/`
-
+    <nav class = "profile-breadcrumb">
+        <span>Dashboard</span>
+        <span>&gt; </span>
+        <span>Select user</span>
+        <span>&gt;</span>
+        <span class = "profile-breadcrumb-active"> Manager user </span>
+        <p>Edit the user info below:</p>
+    </nav> 
 
 `;
 
