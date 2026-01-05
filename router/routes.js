ee33751cdfc20a9322873ad0b60182b7eaa11c85
DYNNIwav <paalomei@gmail.com>
2026-01-05 11:15:10 +0100

    Update routes.js

--- router/routes.js
+++ router/routes.js
@@ -5,7 +5,6 @@ import Login from "../pages/login.js";
 import Contact from "../pages/contact.js";
 import YourResults, { initYourResults } from "../pages/your-results.js";
 import StudentDashboard from "../pages/student-dashboard.js";
-import StudentResults from "../pages/student-results.js";
 import AdminDashboard from "../pages/admin-dashboard.js";
 import Signup, { initSignupForm } from "../pages/signup.js";
 import TeacherDashboard from "../pages/teacher-dashboard.js";
@@ -21,7 +20,6 @@ const routes = [
  { path: "/contact", view: Contact },
  { path: "/your-results", view: YourResults, afterRender: initYourResults },
  { path: "/student-dashboard", view: StudentDashboard },
- { path: "/student-results", view: StudentResults },
  { path: "/admin-dashboard", view: AdminDashboard },
  {
   path: "/signup",
