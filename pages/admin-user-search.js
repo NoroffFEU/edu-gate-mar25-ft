1e93761bc90b7712f7caf2f7e5b30858a5268773
Anna Vestman <anna.v.westman@gmail.com>
2026-01-08 14:02:36 +0100

    Final changes on admin user search + Fixed merge conflict

--- pages/admin-user-search.js
+++ pages/admin-user-search.js
@@ -1,37 +1,29 @@
 export default function adminUserSearch() {
- const existing = document.querySelector("link[data-profile-style]");
- if (!existing) {
-  const link = document.createElement("link");
-  link.rel = "stylesheet";
-  link.href = "./css/admin-user-search.css";
-  link.setAttribute("data-profile-style", "true");
-  link.href =
-   "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css";
-  document.head.appendChild(link);
- }
-
  return /*HTML*/ `
+
   <div class="edit-wrapper">
-    
-      <a href="./admin-dashboard" class="breadcrumb">
-        Dashboard <span>&gt;</span> <strong>Select user</strong>
-      </a>
+      <div class="breadcrumb-top">
+        <a href="#/admin-dashboard" class="breadcrumb-link">Dashboard</a>
+        <span class="arrow">></span>
+        <span class="current">Select user</span>
+    </div>
+      
 <section class="admin-select-user">
       <h1 class="h1-userSearch">Select User</h1>
       <p class="userSearch-sub">
         Search for the user below &amp; select to make changes to their info:
       </p>
 
-      <form class="search-form" role="search" novalidate> 
+      <form class="admin-search-form" role="search" novalidate> 
         <label for="user-search" class="sr-only">Search for user</label>
         <input
-          id="user-search"
+          id="admin-user-search"
           type="search"
           placeholder="Search for user..."
           autocomplete="off"
           required data-required
         />
-        <button type="submit" aria-label="Search"><i class="fa-solid fa-magnifying-glass"></i></button>
+        <button type="submit" aria-label="Search"><img class="admin-search-img" src="./public/icons/search.png" alt="Search icon"></button>
       </form>
 
       <div class="user-table" role="table" aria-label="User list">
@@ -45,7 +37,7 @@ export default function adminUserSearch() {
         </div>
 
        
-        <a class="user-row" href="#">
+        <a class="admin-user-row" href="#">
           <span>1921840</span>
           <span>Joe</span>
           <span>Bloggs</span>
@@ -58,7 +50,7 @@ export default function adminUserSearch() {
           </span>
         </a>
 
-         <a class="user-row" href="#">
+         <a class="admin-user-row" href="#">
           <span>1921840</span>
           <span>Joe</span>
           <span>Bloggs</span>
@@ -71,7 +63,7 @@ export default function adminUserSearch() {
           </span>
         </a>
 
-         <a class="user-row" href="#">
+         <a class="admin-user-row" href="#">
           <span>1921840</span>
           <span>Joe</span>
           <span>Bloggs</span>
@@ -84,7 +76,7 @@ export default function adminUserSearch() {
           </span>
         </a>
 
-         <a class="user-row" href="#">
+         <a class="admin-user-row" href="#">
           <span>1921840</span>
           <span>Joe</span>
           <span>Bloggs</span>
@@ -98,17 +90,17 @@ export default function adminUserSearch() {
         </a>
       </div>
 
-      <nav class="pagination" aria-label="Pagination">
-        <button class="btnSymbol">&laquo;</button>
-        <button class="btnSymbol">&lsaquo;</button>
+      <nav class="admin-pagination" aria-label="Pagination">
+        <button class="btnSymbol"><img src="./public/icons/chevron-double-left.png" alt="Double arrow left"></button>
+        <button class="btnSymbol"><img src="./public/icons/chevron-left.png" alt="Single arrow left"></button>
         <button class="active-admin-search">1</button>
         <button>...</button>
         <button>8</button>
-        <button class="btnSymbol">&rsaquo;</button>
-        <button class="btnSymbol">&raquo;</button>
+        <button class="btnSymbol"><img src="./public/icons/chevron-right.png" alt="Double arrow right"></button>
+        <button class="btnSymbol"><img src="./public/icons/chevron-double-right.png" alt="Single arrow right"></button>
       </nav>
 
-      <a href="./admin-dashboard" class="btn btn-user-search btn--secondary">Back to dashboard</a>
+      <a href="#/admin-dashboard" class="btn btn-user-search btn--secondary">Back to dashboard</a>
     </section>
     </div>
   `;
--- router/routes.js
+++ router/routes.js
@@ -14,7 +14,6 @@ import AdminEdit, { initAdminProfileEdit } from "../pages/admin-edit.js";
 import TeacherProfileEdit, { initTeacherProfileEdit } from "../pages/teacher-profile-edit.js";
 import { initFormValidation } from "../pages/form-field-validation.js";
 import StudentManageUser from "../pages/student-manage-user.js";
-
 import adminUserSearch from "../pages/admin-user-search.js";
 
 const routes = [
