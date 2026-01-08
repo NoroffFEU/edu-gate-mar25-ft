3b19af7f0607be22e0029b63289be4b75663c52b
moaisason <m.isaksson@hotmail.com>
2026-01-08 12:58:13 +0100

    Style admin profile page mobile layout

--- pages/admin-profile.js
+++ pages/admin-profile.js
@@ -2,35 +2,25 @@ export default function AdminProfile() {
     return /*HTML*/ `
 
     <div class="admin-profile-page">
-        <div class="admin-profile-menu">
-            <a href="#/admin-dashboard" data-link>Dashboard</a>
-            <img src= "../public/icons/chevron-right.png"/>
-            <p>Profile</p>
-        </div>
+        <nav class="admin-profile-menu">
+            <a href="#/admin-dashboard" data-link class="admin-profile-a">Dashboard</a>
+            <p>></p>
+            <p class="admin-profile-p">Profile</p>
+        </nav>
         <div class="admin-profile-card">
-            <h1>Profile</h1>
-            <img src= "../public/icons/profile-icon.png"/>
+            <h1 class="admin-profile-h1">Profile</h1>
+            <img src= "../public/icons/profile-icon.png" class="admin-profile-icon" alt="Admin profile icon"/>
             <div class="admin-profile-info">
-                <div class="admin-profile-column">
-                    <span class="admin-profile-label">Name:</span>
+                <span class="admin-profile-label">Name:</span>
                     <span class="admin-profile-value">Joe Bloggs</span>
-                </div>
-                <div class="admin-profile-column">
-                    <span class="admin-profile-label">Email:</span>
+                <span class="admin-profile-label">Email:</span>
                     <span class="admin-profile-value">joeblog2024@edugate.no</span>
-                </div>
-                <div class="admin-profile-column">
-                    <span class="admin-profile-label">DOB:</span>
+                <span class="admin-profile-label">DOB:</span>
                     <span class="admin-profile-value">01/01/1987</span>
-                </div>
-                <div class="admin-profile-column">
-                    <span class="admin-profile-label">Position:</span>
+                <span class="admin-profile-label">Position:</span>
                     <span class="admin-profile-value">Administrator</span>
-                </div>
-                <div class="admin-profile-column">
-                    <span class="admin-profile-label">School:</span>
+                <span class="admin-profile-label">School:</span>
                     <span class="admin-profile-value">The Academy</span>
-                </div>
             </div>
             <a href="#/admin-edit" data-link class="btn btn--secondary">Edit Profile</a>
         </div>
